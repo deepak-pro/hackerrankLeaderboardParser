820b25686793bc14f85508c7d1c2402dbb921a16
Deepak <joshideepak4598@gmail.com>
2020-12-23 23:12:32 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,66 @@
+function formatLink(link){
+	var index = link.indexOf("contests") ;
+	if(index == -1){
+		alert("Invalid Link") ;
+	}
+	var prefix = link.substring(0,index) ;
+	var suffix = link.substring(index-1,link.length) ;
+	var link = prefix + "rest" + suffix + "?offset=0&limit=500" ;
+	return link ;
+}
+
+function getData(){
+	document.getElementById("loading").removeAttribute("hidden") ;
+	document.getElementById("success").setAttribute("hidden","true") ;
+	var link = document.getElementById("leaderboardLink").value ;
+	link = formatLink(link) ;
+	makeRequest(link) ;
+}
+
+function makeRequest(link){
+	var index = link.indexOf("http") ;
+	if(index != -1){
+		link = link.substring(8,link.length) ;
+	}
+	link = "https://cors-anywhere.herokuapp.com/" + link ;
+	console.log("Fetching") ;
+	fetch(link)
+		.then(function (response){
+			return response.json();
+		}).then(function (json){
+			//console.log(json) ;
+			parseJSONData(json) ;
+		}).catch(function (err){
+			alert("Error") ;
+		})
+}
+
+function parseJSONData(jsonData){
+	console.log(jsonData) ;
+	var json = jsonData.models
+	var fields = Object.keys(json[0])
+	var replacer = function(key, value) { return value === null ? '' : value } 
+	var csv = json.map(function(row){
+  		return fields.map(function(fieldName){
+    		return JSON.stringify(row[fieldName], replacer)
+  		}).join(',')
+	})
+	csv.unshift(fields.join(','))
+ 	csv = csv.join('\r\n');
+	console.log(csv)
+
+	download("report.csv",csv) ;
+	document.getElementById("loading").setAttribute("hidden","true") ;
+	document.getElementById("success").removeAttribute("hidden") ;
+
+}
+
+function download(filename, text) {
+  var element = document.createElement('a');
+  element.setAttribute('href', 'data:text/csv;charset=utf-8,' + encodeURIComponent(text));
+  element.setAttribute('download', filename);
+  element.style.display = 'none';
+  document.body.appendChild(element);
+  element.click();
+  document.body.removeChild(element);
+}
